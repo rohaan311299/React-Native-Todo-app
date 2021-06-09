3e431edef375cd2115897db310f996e0f0c4320f
Rohan Kacheria <rohankacheriam@gmail.com>
2021-06-09 19:59:34 +0530

    Alerts and dismiss keyboards

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, FlatList } from 'react-native';
+import { StyleSheet, Text, View, FlatList, Alert, TouchableWithoutFeedback, Keyboard } from 'react-native';
 import Header from "./Components/Header";
 import TodoItem from "./Components/TodoItem";
 import AddTodo from "./Components/AddTodo";
@@ -19,31 +19,45 @@ export default function App() {
   }
 
   const submitHandler=(text)=>{
-    setTodos((prevTodos)=>{
+
+    if(text.length > 3){
+      setTodos((prevTodos)=>{
       return [
           {text:text, key:Math.random().toString() },
           ...prevTodos
       ]
     })
+    }else{
+      Alert.alert('OOPS', "Todos must be over 3 characters long", [
+        {
+          text:"Understood",
+          onPress: ()=>console.log("Alert Closed")
+        }
+      ]);
+    }
   }
 
   return (
-    <View style={styles.container}>
-      <Header />
+    <TouchableWithoutFeedback onPress={()=>{
+      Keyboard.dismiss();
+    }}>
+      <View style={styles.container}>
+        <Header />
 
-      <View style={styles.content}>
-        <AddTodo submitHandler={submitHandler} />
+        <View style={styles.content}>
+          <AddTodo submitHandler={submitHandler} />
 
-        <View style={styles.list}>
-          <FlatList 
-            data={todos}
-            renderItem={({item})=>(
-              <TodoItem item={item} pressHandler={pressHandler} />
-            )}
-          />
+          <View style={styles.list}>
+            <FlatList 
+              data={todos}
+              renderItem={({item})=>(
+                <TodoItem item={item} pressHandler={pressHandler} />
+              )}
+            />
+          </View>
         </View>
       </View>
-    </View>
+    </TouchableWithoutFeedback>
   );
 }
 
